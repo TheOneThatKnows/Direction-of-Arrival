18808c9b6fab19da7e48b9ac57341eae3dc343ff
Alp <107715813+TheOneThatKnows@users.noreply.github.com>
2023-10-03 21:56:34 +0300

    Test Page Uploaded
    This is where the codes are tested

---
+++ TestPage.m
@@ -0,0 +1,79 @@
+clear; clc; close all;
+
+snacd = SNACD(3, 2, 4);
+doa = 90 - asind(0:0.2:0.4) - 5;
+snacd = snacd.Set_Doa_Angles(doa);
+snacd = snacd.Prepare_Array_Manifold();
+snacd = snacd.Simulate(10, 1000, true);
+
+% A_p1 = snacd.A_p1;
+% Phi_2 = snacd.Phi_2;
+% P = snacd.P;
+
+% rhs = A_p1 * Phi_2^(1 - 2*P)
+% PI = zeros(2*P);
+% for i = 1:2*P
+%     PI(i, end-i+1) = 1;
+% end
+% lhs = PI * conj(A_p1)
+
+% ss = DOA.KR_MUSIC_SNACD(snacd.R, snacd.n, snacd.subarray1_locations, snacd.subarray2_locations, 0.5);
+plot(0:0.5:180, 10*log10(snacd.Spatial_Spectrum_Music))
+
+%% CS_Framework
+clear; clc; close all;
+
+DOA = FunctionsOfDOA();
+coef = 0.5; % unit distance between sensors divided by the wavelength of the signal
+
+input = [0 3 3];      % [nested_array M1 M2]
+% input = [1 2 3];      % [coprime_array M1 M2]
+% input = [2 4 3];      % [super_nested_array M1 M2]
+% input = [3 4 4 3];    % [augmented_nested_array_1 M1 M2 L1]
+% input = [4 7 4];      % [augmented_nested_array_2 M1 M2]
+% input = [5 3 3];      % [nested_array_v2 M1 M2]
+% input = [6 2 3 3];    % [sparse_nested_array_with_coprime_displacement_1 N M L]
+% input = [7 1 2 3];    % [sparse_nested_array_with_coprime_displacement_2 N M L]
+
+% M: # of sensors
+if input(1) == 1 % coprime array
+    M = 2 * input(2) - 1 + input(3);
+elseif abs(6.5 - input(1)) < 1 % sparse nested array with coprime displacement
+    M = 2 * input(4);
+else
+    M = sum(input(2:3));
+end
+
+sensor_locations = DOA.Sensor_Locations(input);             % sensor locations
+doa = 90 - asind(0:0.2:0.6) - 5;                            % source angles
+snapshots = 3;                                              % # of snapshots
+SNR_dB = 0;                                                 % signal to noise ratio in decibels
+C = DOA.Mutual_Coupling(100, 0.1, M, sensor_locations);     % mutual coupling
+
+n = length(doa);                                        % number of sources
+s = DOA.Source_Generate(n, snapshots);                  % source signals
+v = DOA.Noise_Generate(SNR_dB, M, snapshots);           % noise
+A = DOA.Array_Manifold(coef, sensor_locations, doa);    % Array Manifold
+
+y = C * A * s + v;
+angles = 0:1:180;
+A = DOA.Array_Manifold(coef, sensor_locations, angles);
+
+CS = CS_Framework(y, A);
+CS = CS.Steepest_Descent_DOA([1 min(1/snapshots, 0.1)]);
+if snapshots ~= 1
+    x = var(CS.Sg.').';
+    figure; plot(angles, abs(x)); title('CS1');
+else
+    figure; plot(angles, abs(CS.Sg)); title('CS1');
+end
+
+% Ry = (1/snapshots) * (y * y');
+% z = Ry(:);
+% [z1, ~] = DOA.Rearrange_According_to_Sensor_Locations(z, sensor_locations);
+% A_d = DOA.khatri_rao(conj(A), A);
+% [A1, ~] = DOA.Rearrange_According_to_Sensor_Locations(A_d, sensor_locations);
+% CS2 = CS_Framework(z, A1);
+% CS = CS.Steepest_Descent_DOA([1 0]);
+% x = var(CS.Sg.').';
+% figure; plot(angles, x); title('CS2');
