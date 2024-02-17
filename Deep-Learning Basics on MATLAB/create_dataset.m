c8e5a7ed38d5640b42f70d2de1b7581b7e4ce56a
Alp <107715813+TheOneThatKnows@users.noreply.github.com>
2024-02-17 20:41:50 +0300

    Dataset Creation
    Dataset is created for the first level of the tree based DNN.

--- Deep-Learning Basics on MATLAB/create_dataset.m
+++ Deep-Learning Basics on MATLAB/create_dataset.m
@@ -8,48 +8,8 @@ coef = 0.5; % unit distance between sensors divided by the wavelength of the sig
 
 % sensor_locations = [0 1 2 6 10 13];
 sensor_locations = 0:63;
-M = length(sensor_locations);
 
-% sensor_locations = DOA.Sensor_Locations(input)                  % sensor locations
-sensor_placement = DOA.Sensor_Placement(sensor_locations)       % sensor places (in order to visualize the array)
-diff_coarray = DOA.Diff_Coarray(sensor_placement)               % difference coarray
-uDOF = DOA.Uniform_Degrees_Of_Freedom(diff_coarray)             % uniform degrees of freedom
-LuDOF = DOA.One_Side_Uniform_Degrees_Of_Freedom(diff_coarray)   % one side uniform degrees of freedom
-
-snapshots = 10000;                                            % # of snapshots
-SNR_dB = 10;                                                % signal to noise ratio in decibels
-C = DOA.Mutual_Coupling(0, 0.1, M, sensor_locations);     % mutual coupling
-
-%%
-doa = [15 20 35];
-K = length(doa);
-A = DOA.Array_Manifold(coef, sensor_locations, doa);
-s = DOA.Source_Generate(K, snapshots);
-n = DOA.Noise_Generate(SNR_dB, M, snapshots);
-y = A * s + n;
-R = (1 / snapshots) * (y * y');
-[e, lambda] = eig(R)
-
-%% 
-figure; hold on;
-for i = M:-1:M-K+1
-    % y = A(:, i) * s(i, :) + n;
-    % R = (1 / snapshots) * (y * y');
-    % [e, lambda] = eig(R)
-    
-    R = lambda(i) * (e(i) * e(i)');
-    
-    ss = DOA.MUSIC(1, coef, R, sensor_locations);
-    plot(0:0.5:180, ss)
-end
-
-%% 
-R = lambda(M) * (e(M) * e(M)');
-    
-ss = DOA.MUSIC(1, coef, R, sensor_locations);
-plot(0:0.5:180, ss)
-
-%% Dataset Preparation I
+%% Dataset Preparation I (This one is not used)
 
 numOfData = sensor_locations(M) * 10000;
 features = zeros((M-1)*M, numOfData);
@@ -115,35 +75,92 @@ end
 % A Novel Tree Model-based DNN to Achieve a High-Resolution DOA Estimation 
 % via Massive MIMO receive array
 
-numOfData = 100;
+numOfData = 100000;
+[~, features_1, labels_1] = TreeDataset(DOA, numOfData, sensor_locations, [30 149 10]);
+[~, features_2_1, labels_2_1] = TreeDataset(DOA, numOfData, sensor_locations, [30 39 1]);
+[~, features_2_2, labels_2_2] = TreeDataset(DOA, numOfData, sensor_locations, [40 49 1]);
+[~, features_2_3, labels_2_3] = TreeDataset(DOA, numOfData, sensor_locations, [50 59 1]);
+[~, features_2_4, labels_2_4] = TreeDataset(DOA, numOfData, sensor_locations, [60 69 1]);
+[~, features_2_5, labels_2_5] = TreeDataset(DOA, numOfData, sensor_locations, [70 79 1]);
+[~, features_2_6, labels_2_6] = TreeDataset(DOA, numOfData, sensor_locations, [80 89 1]);
+[~, features_2_7, labels_2_7] = TreeDataset(DOA, numOfData, sensor_locations, [90 99 1]);
+[~, features_2_8, labels_2_8] = TreeDataset(DOA, numOfData, sensor_locations, [100 109 1]);
+[~, features_2_9, labels_2_9] = TreeDataset(DOA, numOfData, sensor_locations, [110 119 1]);
+[~, features_2_10, labels_2_10] = TreeDataset(DOA, numOfData, sensor_locations, [120 129 1]);
+[~, features_2_11, labels_2_11] = TreeDataset(DOA, numOfData, sensor_locations, [130 139 1]);
+[~, features_2_12, labels_2_12] = TreeDataset(DOA, numOfData, sensor_locations, [140 149 1]);
+
+%% h5 FORMATTING
+
+filename = "features_1.h5";
+h5create(filename,"/DS1",[4032 100000]);
+h5write(filename,"/DS1", features);
+h5disp(filename);
+
+filename = "labels_1.h5";
+h5create(filename,"/DS1",[12 100000]);
+h5write(filename,"/DS1", labels_1);
+h5disp(filename);
+
+%% JSON FORMATTING (did not work)
+
+% Encode the data
+encodedFeatures = jsonencode(features);
+encodedLabels1 = jsonencode(labels_1);
+encodedLabels2 = jsonencode(labels_2);
+
+% Write to a JSON file
+filename = 'features.json';
+fid = fopen(filename, 'w');
+fprintf(fid, '%s', encodedFeatures);
+fclose(fid);
+
+disp(['Data saved to ' filename]);
+
+% Repeat the same process for labels_1
+filename = 'labels1.json';
+fid = fopen(filename, 'w');
+fprintf(fid, '%s', encodedLabels1);
+fclose(fid);
+
+disp(['Data saved to ' filename]);
+
+%% CSV FORMATTING (did not work)
+
+filename = 'features';
+writematrix(features, filename)
+disp(['Data saved to ' filename]);
+
+filename = 'labels1';
+writematrix(labels_1, filename)
+disp(['Data saved to ' filename]);
+
+%% Functions
+
+function [angles, features, labels] = TreeDataset(DOA, numOfData, sensor_locations, phi_specifics)
+angles = zeros(1, numOfData);
+
+M = length(sensor_locations);
 features = zeros((M-1)*M, numOfData);
 
 K = 1; % # of sources is 1
-delta_phi_1 = 10;
-delta_phi_2 = 1;
-phi_max = 149;
-phi_min = 30;
-phi_max_2 = 9;
-phi_min_2 = 0;
-labels_1 = zeros((phi_max - phi_min + 1)/delta_phi_1, numOfData); % (12, numOfData)
-labels_2 = zeros((phi_max_2 - phi_min_2 + 1)/delta_phi_2, numOfData); % (10, numOfData)
+phi_min = phi_specifics(1);
+phi_max = phi_specifics(2);
+delta_phi = phi_specifics(3);
+labels = zeros((phi_max - phi_min + 1)/delta_phi, numOfData);
+
 for idx = 1:numOfData
     doa = rand * (phi_max - phi_min) + phi_min;
+    angles(idx) = doa;
     rounded_doa = round(doa);
 
-    l_1 = ceil((rounded_doa - phi_min + 1) / delta_phi_1);
-    labels_1(l_1, idx) = 1;
-    l_2 = ceil((rem((rounded_doa - phi_min), delta_phi_1) + 1) / delta_phi_2);
-    labels_2(l_2, idx) = 1;
+    l_h = ceil((rounded_doa - phi_min + 1) / delta_phi);
+    labels(l_h, idx) = 1;
 
-    A = DOA.Array_Manifold(coef, sensor_locations, doa);
-    % s = DOA.Source_Generate(K, snapshots);
+    A = DOA.Array_Manifold(0.5, sensor_locations, doa);
     SNR_dB = randn + 10;
     SNR = 10^(SNR_dB / 10);
-    % n = DOA.Noise_Generate(SNR_dB, M, snapshots);
-    % y = A * s + n;
-    % R = (1 / snapshots) * (y * y');
-
+    
     R = A * A' + (1 / SNR) * eye(M);
 
     re_R = real(R);
@@ -162,4 +179,5 @@ for idx = 1:numOfData
     r = (r - mean(r)) / std(r);
 
     features(:, idx) = r;
+end
 end
