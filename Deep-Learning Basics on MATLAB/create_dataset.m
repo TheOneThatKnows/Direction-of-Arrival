98eadc3e5c225d6db592ac550cb8474f52c00470
Alp <107715813+TheOneThatKnows@users.noreply.github.com>
2024-02-18 20:32:05 +0300

    Model is Tested

--- Deep-Learning Basics on MATLAB/create_dataset.m
+++ Deep-Learning Basics on MATLAB/create_dataset.m
@@ -77,30 +77,23 @@ end
 
 numOfData = 100000;
 [~, features_1, labels_1] = TreeDataset(DOA, numOfData, sensor_locations, [30 149 10]);
-[~, features_2_1, labels_2_1] = TreeDataset(DOA, numOfData, sensor_locations, [30 39 1]);
-[~, features_2_2, labels_2_2] = TreeDataset(DOA, numOfData, sensor_locations, [40 49 1]);
-[~, features_2_3, labels_2_3] = TreeDataset(DOA, numOfData, sensor_locations, [50 59 1]);
-[~, features_2_4, labels_2_4] = TreeDataset(DOA, numOfData, sensor_locations, [60 69 1]);
-[~, features_2_5, labels_2_5] = TreeDataset(DOA, numOfData, sensor_locations, [70 79 1]);
-[~, features_2_6, labels_2_6] = TreeDataset(DOA, numOfData, sensor_locations, [80 89 1]);
-[~, features_2_7, labels_2_7] = TreeDataset(DOA, numOfData, sensor_locations, [90 99 1]);
-[~, features_2_8, labels_2_8] = TreeDataset(DOA, numOfData, sensor_locations, [100 109 1]);
-[~, features_2_9, labels_2_9] = TreeDataset(DOA, numOfData, sensor_locations, [110 119 1]);
-[~, features_2_10, labels_2_10] = TreeDataset(DOA, numOfData, sensor_locations, [120 129 1]);
-[~, features_2_11, labels_2_11] = TreeDataset(DOA, numOfData, sensor_locations, [130 139 1]);
-[~, features_2_12, labels_2_12] = TreeDataset(DOA, numOfData, sensor_locations, [140 149 1]);
+% [~, features_2_1, labels_2_1] = TreeDataset(DOA, numOfData, sensor_locations, [30 39 1]);
+% [~, features_2_2, labels_2_2] = TreeDataset(DOA, numOfData, sensor_locations, [40 49 1]);
+% [~, features_2_3, labels_2_3] = TreeDataset(DOA, numOfData, sensor_locations, [50 59 1]);
+% [~, features_2_4, labels_2_4] = TreeDataset(DOA, numOfData, sensor_locations, [60 69 1]);
+% [~, features_2_5, labels_2_5] = TreeDataset(DOA, numOfData, sensor_locations, [70 79 1]);
+% [~, features_2_6, labels_2_6] = TreeDataset(DOA, numOfData, sensor_locations, [80 89 1]);
+% [~, features_2_7, labels_2_7] = TreeDataset(DOA, numOfData, sensor_locations, [90 99 1]);
+% [~, features_2_8, labels_2_8] = TreeDataset(DOA, numOfData, sensor_locations, [100 109 1]);
+% [~, features_2_9, labels_2_9] = TreeDataset(DOA, numOfData, sensor_locations, [110 119 1]);
+% [~, features_2_10, labels_2_10] = TreeDataset(DOA, numOfData, sensor_locations, [120 129 1]);
+% [~, features_2_11, labels_2_11] = TreeDataset(DOA, numOfData, sensor_locations, [130 139 1]);
+% [~, features_2_12, labels_2_12] = TreeDataset(DOA, numOfData, sensor_locations, [140 149 1]);
 
 %% h5 FORMATTING
 
-filename = "features_1.h5";
-h5create(filename,"/DS1",[4032 100000]);
-h5write(filename,"/DS1", features);
-h5disp(filename);
-
-filename = "labels_1.h5";
-h5create(filename,"/DS1",[12 100000]);
-h5write(filename,"/DS1", labels_1);
-h5disp(filename);
+h5_Format(features_1, "features_1", "/DS1");
+h5_Format(labels_1, "labels_1", "/DS1");
 
 %% JSON FORMATTING (did not work)
 
@@ -147,7 +140,8 @@ K = 1; % # of sources is 1
 phi_min = phi_specifics(1);
 phi_max = phi_specifics(2);
 delta_phi = phi_specifics(3);
-labels = zeros((phi_max - phi_min + 1)/delta_phi, numOfData);
+% labels = zeros((phi_max - phi_min + 1)/delta_phi, numOfData);
+labels = zeros(1, numOfData);
 
 for idx = 1:numOfData
     doa = rand * (phi_max - phi_min) + phi_min;
@@ -155,11 +149,13 @@ for idx = 1:numOfData
     rounded_doa = round(doa);
 
     l_h = ceil((rounded_doa - phi_min + 1) / delta_phi);
-    labels(l_h, idx) = 1;
+    % labels(l_h, idx) = 1;
+    labels(idx) = l_h - 1;
 
     A = DOA.Array_Manifold(0.5, sensor_locations, doa);
-    SNR_dB = randn + 10;
-    SNR = 10^(SNR_dB / 10);
+    % SNR_dB = randn + 10;
+    % SNR = 10^(SNR_dB / 10);
+    SNR = 10;
     
     R = A * A' + (1 / SNR) * eye(M);
 
@@ -180,4 +176,11 @@ for idx = 1:numOfData
 
     features(:, idx) = r;
 end
+end
+
+function h5_Format(var, filename, dataset)
+filename = filename + ".h5";
+h5create(filename, dataset, size(var));
+h5write(filename, dataset, var);
+h5disp(filename);
 end
