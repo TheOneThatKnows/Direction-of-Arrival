ccf6894af1c948f7cec1cd85044f50cd6be499c0
Alp <107715813+TheOneThatKnows@users.noreply.github.com>
2024-05-07 15:43:53 +0300

    Sparse Spectrum 1D-CNN

---
+++ Deep-Learning Basics on MATLAB/Sparse Spectrum/create_dataset.m
@@ -0,0 +1,98 @@
+%% Initialization
+
+clear; clc; close all;
+addpath('D:\D\Alp\Master ODTÜ\Thesis\DOA\Codes\Direction-of-Arrival');
+DOA = FunctionsOfDOA();
+
+%% Sensor Properties
+
+sensor_locations = [0 1 4 7 9]; % SLA with 5 sensors
+% sensor_locations = [0 1 2 6 10 13]; % SLA with 6 sensors
+
+%% Dataset Preparation
+
+M = length(sensor_locations);
+N = sensor_locations(M) + 1;
+K = 2;  % # of sources
+
+delta_phi = 1;
+phi_min = 30;
+phi_max = 150;
+angles = phi_min:delta_phi:phi_max;
+Q = length(angles);
+
+numOfData = 43200;
+numOfData = 200;
+features = zeros(Q, 1, numOfData);
+labels = zeros(Q, numOfData);
+
+A_sparse = DOA.Array_Manifold(0.5, sensor_locations, angles);
+for idx = 1:numOfData
+    doa = (-2 * delta_phi) * ones(1, K);
+    i = 1;
+    while true
+        temp_angle = phi_min + rand * (phi_max - phi_min);
+        temp_array = abs(doa - temp_angle);
+        if any(temp_array < delta_phi * 2)
+            i = i - 1;
+        else
+            doa(i) = temp_angle;
+        end
+        if i == K
+            break
+        end
+        i = i + 1;
+    end
+    doa = sort(doa);
+    rounded_doa = round(doa);
+    inds = (rounded_doa - phi_min) / delta_phi + 1;
+    labels(inds, idx) = 1;
+
+    % original case
+    A = DOA.Array_Manifold(0.5, sensor_locations, doa);
+    % L = round(randn * 30 + 500); % # of snapshots
+    L = 70;
+    s = DOA.Source_Generate(K, L);
+    SNR_dB = -5 + sign(randi(3) - 2.5) * 5 + rand * 10;
+    n = DOA.Noise_Generate(SNR_dB, M, L);
+    y = A * s + n;
+    R = (1 / L) * (y * y');
+    
+    z = R(:);
+    A1 = DOA.khatri_rao(conj(A_sparse), A_sparse);
+
+    feature = abs(A1' * z);
+    features(:, 1, idx) = (feature - mean(feature)) / var(feature);
+end
+
+%% 
+
+figure; hold on;
+plot(angles, features(:, 1, numOfData));
+
+[z1, M_v] = DOA.Rearrange_According_to_Sensor_Locations(z, sensor_locations);
+A2 = DOA.Array_Manifold(0.5, -M_v+1:M_v-1, angles);
+
+feature = abs(A2' * z1);
+feature = (feature - mean(feature)) / var(feature);
+
+plot(angles, feature);
+
+legend('z', 'z1')
+
+%% 
+
+pred = predict(net, feature);
+
+figure; hold on;
+plot(angles, feature);
+plot(angles, pred);
+xlabel('angles');
+legend('feature', 'output');
+grid on;
+
+msg = "Actual DOAs:";
+for i = 1:K
+    msg = msg + " " + doa(i);
+end
+title('Feature Spectrum and Output Spectrum (' + msg + ')');
