935a4aa6824900e3d5111205c56b4e7764e07ed7
Alp <107715813+TheOneThatKnows@users.noreply.github.com>
2024-08-26 14:28:51 +0300

    CRN2 v2.4

---
+++ Deep-Learning Basics on MATLAB/CRN v4/Version 2.4/Test_Model.m
@@ -0,0 +1,60 @@
+%% Initialization
+
+clear; clc; close all;
+
+addpath('D:\D\Alp\Master ODTÜ\Thesis\DOA\Codes\Direction-of-Arrival');
+DOA = FunctionsOfDOA();
+
+load CRN_Network_v2_4.mat
+crn_net_v2_4 = net;
+
+clear net
+
+%% Test Case I
+
+Test(crn_net_v2_4, DOA)
+
+%%
+
+function Test(net, DOA)
+sensor_locations = [0 1 4 7 9];
+M = length(sensor_locations);
+N = sensor_locations(M) + 1;
+
+feature = zeros(M, M, 2);
+
+delta_phi = 1;
+phi_min = 30;
+phi_max = 150;
+K = 2;
+doa = DOA.DOA_Generate(K, phi_min, phi_max, delta_phi);
+
+A_ohm = DOA.Array_Manifold(0.5, sensor_locations, doa);
+L = 70; % # of snapshots
+s = DOA.Source_Generate(K, L);
+SNR_dB = -5 + sign(randi(3) - 2.5) * 5 + rand * 10;
+n = DOA.Noise_Generate(SNR_dB, M, L);
+y = A_ohm * s + n;
+R_ohm = (1 / L) * (y * y');
+normalized_R_ohm = R_ohm / max(diag(abs(R_ohm)));
+
+feature(:, :, 1) = real(normalized_R_ohm);
+feature(:, :, 2) = imag(normalized_R_ohm);
+
+spec_2 = predict(net, feature(:, :, 1), feature(:, :, 2), feature).';
+
+angles = 30:1:150;
+spec_1 = DOA.MUSIC(K, 0.5, R_ohm, sensor_locations, angles);
+
+figure; hold on;
+plot(angles, spec_1);
+plot(angles, spec_2 / max(spec_2));
+legend('MUSIC', 'CRN v2.2');
+title_text = "MUSIC Spectrum Comparison, DOAs: ";
+for i = 1:K
+    title_text = title_text + " " + doa(i);
+end
+title(title_text);
+xlabel('angles (deg)');
+ylabel('Amplitude (dB)');
+end
---
+++ Deep-Learning Basics on MATLAB/CRN v4/Version 2.4/create_dataset.m
@@ -0,0 +1,67 @@
+%% Notes for Future
+
+% For normalization, you can use mean and variance of the vectorized form
+% of the covariance matrix
+
+%% Initialization
+
+clear; clc; close all;
+
+addpath('D:\D\Alp\Master ODTÜ\Thesis\DOA\Codes\Direction-of-Arrival');
+DOA = FunctionsOfDOA();
+
+%% Sensor Properties
+
+sensor_locations = [0 1 4 7 9]; % SLA with 5 sensors
+
+%% Dataset Preparation CRN2 (2 Source)
+
+M = length(sensor_locations);
+N = sensor_locations(M) + 1;
+
+numOfData = 1200000;
+% numOfData = 200;
+
+delta_phi = 1;
+phi_min = 30;
+phi_max = 150;
+Q = (phi_max - phi_min) / delta_phi + 1;
+
+features = zeros(M, M, 2, numOfData);
+labels = zeros(Q, numOfData);
+
+for idx = 1:numOfData
+    K = 2;
+    doa = (-2 * delta_phi) * ones(1, K);
+    i = 1;
+    while true
+        temp_angle = phi_min + rand * (phi_max - phi_min);
+        temp_array = abs(doa - temp_angle);
+        if any(temp_array < delta_phi * 2)
+            i = i - 1;
+        else
+            doa(i) = temp_angle;
+        end
+        if i == K
+            break
+        end
+        i = i + 1;
+    end
+    doa = sort(doa);
+    rounded_doa = round(doa);
+
+    inds = (rounded_doa - phi_min) / delta_phi + 1;
+    labels(inds, idx) = 100;
+
+    A_ohm = DOA.Array_Manifold(0.5, sensor_locations, doa);
+    L = 70; % # of snapshots
+    s = DOA.Source_Generate(K, L);
+    SNR_dB = -5 + sign(randi(3) - 2.5) * 5 + rand * 10;
+    n = DOA.Noise_Generate(SNR_dB, M, L);
+    y = A_ohm * s + n;
+    R_ohm = (1 / L) * (y * y');
+    normalized_R_ohm = R_ohm / max(diag(abs(R_ohm)));
+
+    features(:, :, 1, idx) = real(normalized_R_ohm);
+    features(:, :, 2, idx) = imag(normalized_R_ohm);
+end
