c07d242d01c9193395b06509cc67703e37487fa0
Alp <107715813+TheOneThatKnows@users.noreply.github.com>
2024-11-03 23:26:50 +0300

    Update

--- sparse_arrays_v2.m
+++ sparse_arrays_v2.m
@@ -10,8 +10,8 @@ coef = 0.5; % unit distance between sensors divided by the wavelength of the sig
 % input = [1 2 3];      % [coprime_array M1 M2]
 % input = [2 7 4];      % [super_nested_array M1 M2]
 % input = [3 4 4 3];    % [augmented_nested_array_1 M1 M2 L1]
-input = [4 7 4];      % [augmented_nested_array_2 M1 M2]
-% input = [5 3 3];      % [nested_array_v2 M1 M2]
+% input = [4 7 4];      % [augmented_nested_array_2 M1 M2]
+input = [5 3 3];      % [nested_array_v2 M1 M2]
 % input = [6 2 3 3];    % [sparse_nested_array_with_coprime_displacement_1 N M L]
 % input = [7 1 2 3];    % [sparse_nested_array_with_coprime_displacement_2 N M L]
 
@@ -26,15 +26,17 @@ end
 
 %%
 
-M = 6;
+% M = 6;
 % sensor_locations = 0:5;
-sensor_locations = [0 1 2 6 10 13];
+% sensor_locations = [0 1 2 6 10 13];
 
-% sensor_locations = DOA.Sensor_Locations(input)                  % sensor locations
+sensor_locations = DOA.Sensor_Locations(input)                  % sensor locations
 sensor_placement = DOA.Sensor_Placement(sensor_locations)       % sensor places (in order to visualize the array)
-diff_coarray = DOA.Diff_Coarray(sensor_placement)               % difference coarray
-uDOF = DOA.Uniform_Degrees_Of_Freedom(diff_coarray)             % uniform degrees of freedom
-LuDOF = DOA.One_Side_Uniform_Degrees_Of_Freedom(diff_coarray)   % one side uniform degrees of freedom
+diff_coarray = DOA.Diff_Coarray(sensor_locations)               % difference coarray
+uDOF = DOA.Uniform_Degrees_Of_Freedom(sensor_locations)             % uniform degrees of freedom
+LuDOF = DOA.One_Side_Uniform_Degrees_Of_Freedom(sensor_locations)   % one side uniform degrees of freedom
+
+%%
 
 % Now, let's use the calculated sensor_locations to compute the array pattern
 DOA.Array_Pattern(sensor_locations, coef)
